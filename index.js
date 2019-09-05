7a5c6e8c5501739a18506754c44f6d4b60460b09
yakhousam <42283612+yakhousam@users.noreply.github.com>
2019-09-05 17:33:42 +0100

    fix client folder

--- index.js
+++ index.js
@@ -51,9 +51,9 @@ if (dev) {
    
   });
 } else {
-  app.use(express.static(path.join(__dirname , "../build")));
+  app.use(express.static(path.join(__dirname , "/client/build")));
   app.get("/", restricAccess, (req, res) => {
-    res.sendFile(path.join(__dirname , "/client/index.html"));
+    res.sendFile(path.join(__dirname , "/client/build/index.html"));
   });
 }
 app.get("/api/getuserinfos", restricAccess, (req, res) => {
