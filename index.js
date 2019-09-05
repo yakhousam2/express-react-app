dfeb1196669335d017ceb0c4e689906365742e8a
yakhousam <42283612+yakhousam@users.noreply.github.com>
2019-09-05 20:05:21 +0100

    static

--- index.js
+++ index.js
@@ -57,9 +57,10 @@ if (dev) {
    
   });
 } else {
-  app.use(express.static(path.join(__dirname , "client", "build")));
+  app.use(express.static(path.join(__dirname , "client/build")));
+  app.use(express.static(__dirname ));
   app.get("/", restricAccess, (req, res) => {
-    res.sendFile(path.join(__dirname , "client", "build","index.html"));
+    res.sendFile(path.join(__dirname , "client/build","index.html"));
   });
 }
 app.get("/api/getuserinfos", restricAccess, (req, res) => {
